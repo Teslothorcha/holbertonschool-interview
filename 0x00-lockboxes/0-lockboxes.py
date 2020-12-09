b0b28a94a13fd701ed44a4383293fac6c70e862b
teslothorcha <juandavidmarinbernal@gmail.com>
2020-12-08 22:27:27 -0500

    took more tha it nedded XD

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Validates if all boxes can be openned
+    @boxes: list of lists
+    Returns: True or False
+"""
+
+def canUnlockAll(boxes):
+    to_open = [i for i in range(len(boxes))]
+    opened = {"0": "ok"}
+    can = 0
+    for box in range(len(boxes)):
+        for key in boxes[box]:
+            if key in to_open and key != box:
+                if key not in opened:
+                    opened["{}".format(key)] = "ok"
+    for k in to_open:
+        if str(k) not in opened:
+            return False
+    return True
